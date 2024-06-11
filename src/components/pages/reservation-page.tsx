35849a97c643b5187dd25fd3fc7ef68439861319
Kacper Płusa <48603149+kplusa@users.noreply.github.com>
2024-06-11 22:08:14 +0200

    Feat/booking page - Add bookings table (#3)
    * layout - Stretch the content to fill the full w & h
    
    * Booking page - Add booking table

--- src/components/pages/reservation-page.tsx
+++ src/components/pages/reservation-page.tsx
@@ -1,9 +1,20 @@
 import Layout from "../layout";
+import { bookingsMockData } from "@/mocks/booking/bookings-mock-data";
+import { Booking, columns } from "@/bookings/columns";
+import { DataTable } from "../ui/data-table";
 
+function getData(): Booking[] {
+  // TODO: retrieving data using API
+  return bookingsMockData;
+}
 const ReservationPage = () => {
-  return <Layout>
-    This is Reservation page
-  </Layout>;
+  const data = getData();
+
+  return (
+    <Layout>
+      <DataTable columns={columns} data={data} />
+    </Layout>
+  );
 };
 
 export default ReservationPage;
