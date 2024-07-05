b2778aeb752fcf6c46f401f4976036419087a68f
BuildTools <unconfigured@null.spigotmc.org>
2024-07-05 19:18:33 +0200

    add endpoints

---
+++ src/connection/api-config.ts
@@ -0,0 +1,11 @@
+export const USERS = "/users";
+export const USERS_REGISTER = "/users/register";
+export const USERS_LOGIN = "/users/login";
+export const USERS_JWT = "/users/jwt";
+
+export const BOATS = "/boats";
+export const BOATS_DAMAGE = "/boatsDamage";
+
+export const BOOKING = "/booking";
+export const BOOKING_HISTORY = "/booking/historia-rezerwacji";
+export const BOOKING_ADD = "/booking/dodaj-rezerwacje";
